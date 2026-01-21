dc06f1fc64314dada68c28898d8bbb758c613a47
priyansh-goel <priyanshgoel548@gmail.com>
2026-01-21 17:38:33 +0530

    Fullstack deploy setup

---
+++ 1-actual-backend/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require("path");
+
+const { getStoredItems, storeItems } = require('./data/items');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/items', async (req, res) => {
+  const storedItems = await getStoredItems();
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+  res.json({ items: storedItems });
+});
+
+app.get('/items/:id', async (req, res) => {
+  const storedItems = await getStoredItems();
+  const item = storedItems.find((item) => item.id === req.params.id);
+  res.json({ item });
+});
+
+app.post('/items', async (req, res) => {
+  const existingItems = await getStoredItems();
+  const itemData = req.body;
+  const newItem = {
+    ...itemData,
+    id: Math.random().toString(),
+  };
+  const updatedItems = [newItem, ...existingItems];
+  await storeItems(updatedItems);
+  res.status(201).json({ message: 'Stored new item.', item: newItem });
+});
+
+// ✅ Serve React build folder
+app.use(express.static(path.join(__dirname, "../build")));
+
+// ✅ React routing support (for React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log("Server running on " + PORT));
